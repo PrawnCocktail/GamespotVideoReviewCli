0bc9004c3728e270155faee8e12d8d2088a1de9a
PrawnCocktail <5621958+PrawnCocktail@users.noreply.github.com>
2020-08-05 02:35:05 +0100

    Add project files.

---
+++ GamespotVideoReviewCli/Program.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamespotcli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentResults = 0;
+            jsonClass.Rootobject gamespotJson = getJson(currentResults, 1);
+            Console.WriteLine(gamespotJson.number_of_total_results + " video reviews found.");
+            List<CSVClass> reviewList = new List<CSVClass>();
+
+            while (currentResults <= gamespotJson.number_of_total_results)
+            {
+                gamespotJson = getJson(currentResults, 100);
+
+                foreach (var item in gamespotJson.results)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("###########################################################################");
+                    Console.WriteLine();
+
+                    CSVClass itemObj = new CSVClass();
+
+                    itemObj.Title = item.title;
+                    Console.WriteLine(item.title);
+
+                    foreach (var ass in item.associations)
+                    {
+                        JObject jass = ass as JObject;
+
+                        if (jass != null)
+                        {
+                            jsonClass.Association assresult = jass.ToObject<jsonClass.Association>();
+                            itemObj.GameName = assresult.name;
+                            Console.WriteLine(itemObj.GameName);
+                        }
+                    }
+
+
+                    itemObj.PublishDate = item.publish_date;
+                    Console.WriteLine(item.publish_date);
+
+                    itemObj.Blurb = item.deck;
+                    Console.WriteLine(item.deck);
+
+                    itemObj.HighVideo = item.high_url;
+                    Console.WriteLine(item.high_url);
+
+                    itemObj.LowVideo = item.low_url;
+                    Console.WriteLine(item.low_url);
+
+                    itemObj.HDVideo = item.hd_url;
+                    Console.WriteLine(item.hd_url);
+
+                    itemObj.ReviewUrl = item.site_detail_url;
+                    Console.WriteLine(item.site_detail_url);
+
+                    reviewList.Add(itemObj);
+                }
+
+                WriteCSV(reviewList, @"VideoReviews.csv");
+                reviewList.Clear();
+                currentResults = currentResults + 100;
+            }
+        }
+
+        static jsonClass.Rootobject getJson(int offset, int limit)
+        {
+            string apiKey = "YOUR GAMESPOT APIKEY";
+            using (var client = new WebClient())
+            {
+                var settings = new JsonSerializerSettings();
+                settings.NullValueHandling = NullValueHandling.Ignore;
+                settings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                string json = client.DownloadString("https://www.gamespot.com/api/videos/?api_key=" + apiKey + "&format=json&filter=categories:122&offset=" + offset + "&limit=" + limit);
+                return JsonConvert.DeserializeObject<jsonClass.Rootobject>(json, settings);
+            }
+        }
+
+        static void WriteCSV<T>(IEnumerable<T> items, string path)
+        {
+            Type itemType = typeof(T);
+            var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            bool writeHeader = false;
+
+            if (!File.Exists(path))
+            {
+                writeHeader = true;
+            }
+
+            using (var writer = new StreamWriter(path, true))
+            {
+                if (writeHeader == true)
+                {
+                    writer.WriteLine(string.Join("; ", props.Select(p => p.Name)));
+                }
+
+                foreach (var item in items)
+                {
+                    writer.WriteLine(string.Join("; ", props.Select(p => p.GetValue(item, null))));
+                }
+            }
+        }
+    }
+}
---
+++ GamespotVideoReviewCli/jsonClass.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamespotcli
+{
+
+    class CSVClass
+    {
+        public string Title { get; set; }
+        public string GameName { get; set; }
+        public string PublishDate { get; set; }
+        public string Blurb { get; set; }
+        public string HighVideo { get; set; }
+        public string LowVideo { get; set; }
+        public string HDVideo { get; set; }
+        public string ReviewUrl { get; set; }
+    }
+
+    class jsonClass
+    {
+        public class Rootobject
+        {
+            public string error { get; set; }
+            public int limit { get; set; }
+            public int offset { get; set; }
+            public int number_of_page_results { get; set; }
+            public int number_of_total_results { get; set; }
+            public int status_code { get; set; }
+            public List<Result> results { get; set; }
+            public string version { get; set; }
+        }
+
+        public class Result
+        {
+            public int length_seconds { get; set; }
+            public string publish_date { get; set; }
+            public string mpx_id { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public Image image { get; set; }
+            public string deck { get; set; }
+            public string source { get; set; }
+            public Category[] categories { get; set; }
+            public List<object> associations { get; set; }
+            public string high_url { get; set; }
+            public string low_url { get; set; }
+            public string hd_url { get; set; }
+            public string site_detail_url { get; set; }
+        }
+
+        public class Image
+        {
+            public string square_tiny { get; set; }
+            public string screen_tiny { get; set; }
+            public string square_small { get; set; }
+            public string original { get; set; }
+        }
+
+        public class Category
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Association
+        {
+            public string name { get; set; }
+        }
+    }
+}
